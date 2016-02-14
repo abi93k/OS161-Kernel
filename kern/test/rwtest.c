c95cfa8b5b6cbe538fd710df08171a78f08f87bf
Abhishek Kannan <akannan4@buffalo.edu>
2016-02-14 12:09:00 -0500

    implemented reader writer lock test

--- kern/test/rwtest.c
+++ kern/test/rwtest.c
@@ -13,16 +13,77 @@
 #include <spinlock.h>
 
  #define CREATELOOPS		8
+ #define NREADERTHREADS      32
+ #define NWRITERTHREADS      5
 
 static struct semaphore *donesem = NULL;
 static struct rwlock *testrwlock = NULL;
 static bool test_status = FAIL;
 
+int n_reader_threads_accessing;
+int n_writer_threads_accessing;
+
+static
+void
+rwlock_reader_thread(void *junk, unsigned long num)
+{
+	(void)junk;
+	
+	kprintf_t(".");
+	rwlock_acquire_read(testrwlock);
+
+	n_reader_threads_accessing++;
+
+	kprintf("Reader thread %lu \n",num);
+
+	// check if no writers have access and no more than MAX_READER threads are acessing critical section :)
+	if(n_writer_threads_accessing > 0 || n_reader_threads_accessing > MAX_READERS) {
+		test_status=FAIL;
+	}
+
+	n_reader_threads_accessing--;
+
+	rwlock_release_read(testrwlock);
+
+	V(donesem);
+	return;
+}
+
+static
+void
+rwlock_writer_thread(void *junk, unsigned long num)
+{
+	(void)junk;
+	
+	kprintf_t(".");
+	rwlock_acquire_write(testrwlock);
+
+	n_writer_threads_accessing++;
+
+	kprintf("Writer thread %lu \n",num);
+
+	// check if I have exclusice access :)
+	if(n_reader_threads_accessing>0 || n_writer_threads_accessing > 1) {
+		test_status=FAIL;
+	}
+
+	n_writer_threads_accessing--;
+
+	rwlock_release_write(testrwlock);
+	
+
+	V(donesem);
+	return;
+}
+
+
 int rwtest(int nargs, char **args) {
 	(void)nargs;
 	(void)args;
 
-	int i;
+	int i,result;
+	n_reader_threads_accessing=0;
+	n_writer_threads_accessing=0;
 
 	kprintf_n("Starting rwt1...\n");
 	// Check if rwlock gets destroyed properly
@@ -43,6 +104,28 @@ int rwtest(int nargs, char **args) {
 	}
 	test_status = SUCCESS;
 
+	// creating reader threads
+	for (i=0; i<NREADERTHREADS; i++) {
+		kprintf_t(".");
+		result = thread_fork("synchtest", NULL, rwlock_reader_thread, NULL, i);
+		if (result) {
+			panic("rwt1: thread_fork failed: %s\n", strerror(result));
+		}
+	}
+
+	// creating writer threads
+	for (i=0; i<NWRITERTHREADS; i++) {
+		kprintf_t(".");
+		result = thread_fork("synchtest", NULL, rwlock_writer_thread, NULL, i);
+		if (result) {
+			panic("rwt1: thread_fork failed: %s\n", strerror(result));
+		}
+	}
+
+	for (i=0; i<NREADERTHREADS+NWRITERTHREADS; i++) {
+		kprintf_t(".");
+		P(donesem);
+	}
 	success(test_status, SECRET, "rwt1");
 
 	return 0;
