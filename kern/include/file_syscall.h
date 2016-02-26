ce76e551ae147d937aab456c8eca4f6f7069090d
Abhishek Kannan <akannan4@buffalo.edu>
2016-02-26 02:28:42 -0500

    added todo stubs for file system calls

--- kern/include/file_syscall.h
+++ kern/include/file_syscall.h
@@ -22,15 +22,14 @@ struct fdesc
 int sys_open(/* Define appropriate arguments */);
 int sys_close(/* Define appropriate arguments */);
 
-int sys_read(/* Define appropriate arguments */);
-int sys_write(/* Define appropriate arguments */);
+ssize_t sys_read(int fd, void* buf, size_t buflen) ;
+ssize_t sys_write(int fd, const void *buf, size_t nbytes);
 
 int sys_dup2(/* Define appropriate arguments */);
-int sys_lseek(/* Define appropriate arguments */);
+off_t lseek(int fd, off_t pos, int whence);
 
 int sys_chdir(/* Define appropriate arguments */);
-int sys___getcwd(/* Define appropriate arguments */);
-
+int __getcwd(char *buf, size_t buflen);
 
 #endif
 
