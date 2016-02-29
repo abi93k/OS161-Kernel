5e274a7c5c26ae4902f69c30bcf52776ea0c2625
Abhishek Kannan <abi93k@gmail.com>
2016-02-28 22:29:58 -0500

    implemented sys_lseek file system call

--- kern/syscall/file_syscall.c
+++ kern/syscall/file_syscall.c
@@ -102,8 +102,50 @@ sys_write(int fd, const void *buf, size_t nbytes)
 }
 
 off_t
-lseek(int fd, off_t pos, int whence) {
-	/* TODO */
+sys_lseek(int fd, off_t pos, int whence) 
+{
+
+	struct iovec iov;
+	struct uio u;
+	off_t offset = 0;
+	struct stat f_stat;
+
+	if (fd < 0 || fd > OPEN_MAX) 
+		return EBADF; 								// not a valid file descriptor
+
+	struct fdesc * t_fd_entry = curthread->t_fdtable[fd];
+
+	if(t_fd_entry == -1)
+		return EBADF; 								// not a valid file descriptor
+
+	if( (whence == SEEK_SET || whence == SEEK_CUR || whence == SEEK_END) == 0)
+		return EINVAL;								// whence is invalid
+
+	lock_acquire(t_fd_entry->lock);
+	if(whence == SEEK_SET) {
+		offset = pos;
+	}
+	else if(whence == SEEK_CUR) {
+		offset = t_fd_entry->offset + pos;
+	}
+	else if (whence == SEEK_END) {
+		result = VOP_STAT(t_fd_entry->vn, &f_stat);	// get file size to determine end of file
+		if(result) {
+			lock_release(t_fd_entry->lock);
+			return result;
+		}
+		offset = f_stat.st_size + pos;				// end of file + position
+	}
+
+	result = VOP_TRYSEEK(t_fd_entry->vn, offset);
+	if(result) {
+		lock_release(t_fd_entry->lock);
+		return result;
+	}
+	t_fd_entry->offset = offset;
+	lock_release(t_fd_entry->lock);
+
+	return offset;
 }
 
 int
@@ -122,7 +164,7 @@ sys__getcwd(char *buf, size_t buflen)
 	u.uio_iov = &iov;
 	u.uio_iovcnt = 1;
 	u.uio_resid = buflen;					// amount to read from the file
-	u.uio_offset = 0;		// offset 
+	u.uio_offset = 0;						// offset 
 	u.uio_segflg = UIO_USERISPACE;
 	u.uio_rw = UIO_READ;					// READ or WRITE ?
 	u.uio_space = curthread->t_addrspace;	// address space of thread
